c81568faf25d7b3324a91481773e088c1d3c24bd
dannieatus <annieatus@gmail.com>
2015-10-21 22:48:04 -0700

    initial checkin

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+include('header.html');
+?>
+ 
+ 
+    
+        <!-- this is main body beginning -->
+        <div id="mainbody" >
+
+            
+            <div id="banner">
+                <img src="img/banner.jpg">
+            </div>
+
+            
+            <div class="topbanner">
+                <h1>WHY US</h1>
+                <ul>
+                    <li>Experience the newest and hottest way to improve your home! Let's introduce "Timeshare" system to you!</li>
+                    <li>The lowest price in town!</li>
+                    <li>Order once, get service one month!</li>
+                </ul>
+            </div>
+
+             <div class="topbanner">
+                <h1>HOW TO USE</h1>
+                <ul>
+                    <li>Search</li>
+                    <li>Order</li>
+                    <li>Get service!</li>
+                </ul>            
+            </div>
+            <div class="topbanner">
+                <h1>COUPONS</h1>
+                <ul>
+                    <li>Computer</li>
+                    <li>House Cleaning</li>
+                </ul>
+            </div>
+            
+            <div class="bottombanner">
+                <h2>House Services</h2>
+                <ul>
+                    <li>House Cleaning</li>
+                    <li>Gardening</li>
+                    <li>Plumbing</li>
+                    <li>Repairing</li>
+                    <li>More..</li>
+                </ul>
+            </div>
+            <div class="bottombanner">
+                <h2>General Services</h2>
+                <ul>
+                    <li>Walking Dog</li>
+                    <li>Drive Instead</li>
+                    <li>Grocery Purchase</li>
+                    <li>Move in &#124; Move out</li>
+                    <li>More..</li>
+                </ul>
+            </div>
+            <div class="bottombanner">
+                <h2>Cooking Services</h2>
+                <ul>
+                    <li>Frence</li>
+                    <li>Chinese</li>
+                    <li>Japanese</li>
+                    <li>Indian</li>
+                </ul>
+            </div>
+            <div class="bottombanner">
+                <h2>Tutor Services</h2>
+                <ul>
+                    <li>Spanish</li>
+                    <li>Swim</li>
+                    <li>Drive</li>
+                    <li>Computer</li>
+                    <li>More..</li>
+                </ul>    
+            </div>          
+        </div>   
+        <!-- this is main body end -->
+
+
+<?php
+include('foot.html');
+?>
