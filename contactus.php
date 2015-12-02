aca87755578787c80bf6fb0eed1e96dde685d918
CHENG-KAI <k20052@yahoo.com.tw>
2015-12-02 09:21:36 +0800

    modify contactus page

--- contactus.php
+++ contactus.php
@@ -3,14 +3,14 @@
 ?>
 <form id="contact" action="mailto:lyan@scu.edu" enctype="text/plain" method="post">
     <fieldset>
-        <legend class="contactuslegend" >Please email us if you have any quetion.</legend>
+        <legend>Please email us if you have any quetion.</legend>
         <label class="contactusheading" for="name">Subject:</label>
         <input name="Subject" type="text" size ="40">
         <br/>
         <label class="contactusheading" for="content">Comments:</label>
         <textarea name="content" rows="5" cols="40"></textarea>
         <br>
-        <input id="contactus_submit" type="submit" value="Submit"><br/>
+        <input id="contactus_submit"type="submit" value="Submit"><br/>
     </fieldset>
 </form>
 <?php
